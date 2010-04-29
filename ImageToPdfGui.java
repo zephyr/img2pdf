fe5cf849d07bd7fdb94fb8a8f1c0cc82ec7323bd
Dennis Heidsiek <HeidsiekB@aol.com>
2010-04-29 20:42:36 +0200

    Improved code reuse.

--- ImageToPdfGui.java
+++ ImageToPdfGui.java
@@ -6,40 +6,48 @@ import javax.swing.*;
  * @author Dennis Heidsiek
  */
 public class ImageToPdfGui {
+
+  protected static JLabel createLabel(String title, FileDrop.Listener listener) {
+    JLabel label = new JLabel(String.format(
+        "<html><centering><h1>%s</h1>Just drag and drop<br/>your files at this text!</centering></html>",
+        title));
+      
+    label.setFont(new Font("Dialog", Font.PLAIN, 24));
+    new FileDrop(System.out, label, BorderFactory.createRaisedBevelBorder(), listener);
+    return label;
+  }
   
   public static void main(String... arg) throws Exception {
-    Font font = new Font("Dialog", Font.PLAIN, 24);
-    
-    JLabel left = new JLabel("<html><h1>Create single PDF files</h1>Just drag and drop<br/>your files at this text!</html>");
-    left.setFont(font);
-    new FileDrop( System.out, left, /*dragBorder,*/ new FileDrop.Listener(){
-        public void filesDropped(File[] files) {
-          for(File f : files) {
-            try {
-              System.out.format("%s%n", f.getCanonicalPath());
-            } catch(IOException e) {}
-          }
-        }
-    });
-    JLabel right = new JLabel("<html><h1>Create only one PDF file</h1>Just drag and drop<br/>your files at this text!</html>");
-    right.setFont(font);
-    new FileDrop( System.out, right, /*dragBorder,*/ new FileDrop.Listener(){
-        public void filesDropped(File[] files) {
-          for(File f : files) {
-            try {
-              System.out.format("%s%n", f.getCanonicalPath());
-            } catch(IOException e) {}
-          }
-        }
-    });
-
     
     JFrame frame = new JFrame("img2pdf");
     frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     frame.setLayout(new GridLayout(1, 2));
-    frame.add(left);
-    frame.add(right);
-    frame.pack();
+    
+    frame.add(createLabel("Create single PDF files",
+      new FileDrop.Listener(){
+        public void filesDropped(File[] files) {
+          for(File f : files) {
+            try {
+              System.out.format("%s%n", f.getCanonicalPath());
+            } catch(IOException e) {}
+          }
+        }
+      }
+    ));
+    
+    frame.add(createLabel("Create only one PDF file",
+      new FileDrop.Listener(){
+        public void filesDropped(File[] files) {
+          for(File f : files) {
+            try {
+              System.out.format("%s%n", f.getCanonicalPath());
+            } catch(IOException e) {}
+          }
+        }
+      }
+    ));
+    
+    frame.setSize(600,220);
     frame.setVisible(true);
   }
   
